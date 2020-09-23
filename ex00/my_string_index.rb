8ca8b492a2ffb48f9eda901b9eb921579e6a93c8
ibrahim bashir <bashir_i@qwasar.io>
2020-09-23 10:34:10 +0000

    edited the code to use iterator

--- ex00/my_string_index.rb
+++ ex00/my_string_index.rb
@@ -1,6 +1,10 @@
 def my_string_index(haystack, needle)
   haystack_array = haystack.split('')
   haystack_array.each_with_index do |character, index|
-    return index if character == needle
+    if character == needle
+         return index 
+    else
+        return -1
+    end
   end
 end
