56384507c3c3920f719d62022d2163e9bc7178ef
ibrahim bashir <bashir_i@qwasar.io>
2020-09-23 10:22:08 +0000

    Quest04: ex00

--- ex00/my_string_index.rb
+++ ex00/my_string_index.rb
@@ -1,3 +1,6 @@
-def my_string_index(str, n)
-  return str.index(n)
+def my_string_index(haystack, needle)
+  haystack_array = haystack.split('')
+  haystack_array.each_with_index do |character, index|
+    return index if character == needle
+  end
 end
