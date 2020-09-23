724534cb3b2e8904ab3c55c5c28592aac2da6eaf
ibrahim bashir <bashir_i@qwasar.io>
2020-09-23 10:59:23 +0000

    fixed error: ex00

--- ex00/my_string_index.rb
+++ ex00/my_string_index.rb
@@ -1,10 +1,8 @@
 def my_string_index(haystack, needle)
   haystack_array = haystack.split('')
-  haystack_array.each_with_index do |character, index|
-    if character == needle
-         return index 
-    else
-        return -1
-    end
+  index = -1
+  haystack_array.each_with_index do |character, i|
+     index = character == needle ? i : index
   end
+  return index
 end
