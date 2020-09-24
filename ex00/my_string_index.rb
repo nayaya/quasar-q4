9d194a8ccfe2346fd61ebafc2bd70768ba03a10c
ibrahim bashir <bashir_i@qwasar.io>
2020-09-24 07:55:29 +0000

    refactored ex00 again

--- ex00/my_string_index.rb
+++ ex00/my_string_index.rb
@@ -1,6 +1,10 @@
 def my_string_index(param_1, param_2)
   haystack_array = param_1.split('')
-  for character in haystack_array do
-    return haystack_array.index(character) if character == param_2
+  index = -1
+  haystack_array.each do |character|
+    if character == param_2
+        index = haystack_array.index(character)
+    end
   end
+  return index
 end
